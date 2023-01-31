75141d5fa67d692fe6c9b7aa06df91f968a42783
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2023-01-31 15:36:23 +0100

    added RC3

--- scripts/repo.sh
+++ scripts/repo.sh
@@ -13,6 +13,7 @@ REPO=tx-portal-assets
 BASE=https://api.github.com
 BASE_RAW=https://raw.githubusercontent.com
 BRANCH=main
+DOC_BASE=./public/documentation
 
 tx-asset-tags() {
     curl -s "$BASE/repos/$OWNER/$REPO/git/refs/tags"
@@ -24,4 +25,20 @@ tx-asset-contents() {
 
 tx-asset-raw() {
     curl -s "$BASE_RAW/$OWNER/$REPO/$BRANCH/$1"
+}
+
+tx-asset-get-latest() {
+    #git checkout main
+    #git pull
+    tx-asset-tags > ${DOC_BASE}/data/Releases.json
+    LATEST=$(git tag | tail -1)
+    echo $LATEST
+}
+
+tx-asset-checkout-tag() {
+    LATEST=$(tx-asset-get-latest)
+    TAG=${1:-$LATEST}
+    git checkout tags/${TAG} -b tags/${TAG}
+    mkdir ${DOC_BASE}/data/${TAG}
+    npx directory-tree docs > ${DOC_BASE}/data/${TAG}/Tree.json
 }
