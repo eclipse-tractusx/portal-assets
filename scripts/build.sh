f84bb440ab1db570c3dcfb12fdc868fb954b6c11
Evelyn Gurschler <evelyn.gurschler@bmw.de>
2023-05-25 12:02:50 +0200

    chore: remove spaces

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,7 +21,7 @@
 
 copy-dist() {
     mkdir -p ./public/documentation/js/lib/$1
-    cp -R ./node_modules/$1/dist/* ./public/documentation/js/lib/$1    
+    cp -R ./node_modules/$1/dist/* ./public/documentation/js/lib/$1
 }
 
 copy-dist zero-md
